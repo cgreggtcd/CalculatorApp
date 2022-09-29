4b823bf04407f1e40293b03cd8f9c7731590bcfd
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2022-09-29 14:31:23 +0100

    Add recursive calculation of any multiplication.

--- calculator.py
+++ calculator.py
@@ -1,2 +1,30 @@
 def calculate(input):
-    return 0
+    answer = multiply_string(input)
+    return int(answer)
+
+def multiply_string(input):
+    index_of_mul = input.find("*")
+    print("Input: {}".format(input))
+    print("index_of_mul: {}".format(index_of_mul))
+
+    # If there is no * in the equation
+    if index_of_mul == -1:
+        return input
+
+    # Find the start of the multiplicand
+    start_multiplicand = index_of_mul - 1
+    while start_multiplicand > 0 and input[start_multiplicand].isdigit():
+        start_multiplicand -= 1
+    # Calculate the multiplicand
+    multiplicand = int(input[start_multiplicand:index_of_mul])
+
+    # Find the end of the multiplier
+    end_multiplier = index_of_mul + 1
+    while end_multiplier < len(input) and input[end_multiplier].isdigit():
+        end_multiplier += 1
+    # Calculate the multiplier
+    multiplier = int(input[index_of_mul+1:end_multiplier])
+
+    # Recursively call the method, replacing the part that was calculated with the answer
+    return multiply_string(input[:start_multiplicand]
+        + str(multiplicand * multiplier) + input[end_multiplier:])
