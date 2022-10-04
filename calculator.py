40d11565016f637c616617dc578e2c2c569eb69c
Joe <handjo@tcd.ie>
2022-10-04 15:24:20 +0100

    Addition-Implementation
    Addition implemented, along with test cases

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 def calculate(input):
+    answer = add_string(input)
     answer = multiply_string(input)
     return int(answer)
 
@@ -26,3 +27,31 @@ def multiply_string(input):
     # Recursively call the method, replacing the part that was calculated with the answer
     return multiply_string(input[:start_multiplicand]
         + str(multiplicand * multiplier) + input[end_multiplier:])
+
+def add_string(input):
+    # using similar naming convention as the multiplication function
+    index_of_add = input.find("+")
+
+    # If no addition operations are found
+    if index_of_add == -1:
+        return input
+
+    # Find the augend (number to which another is added)
+    start_augend = index_of_add - 1
+    while start_augend > 0 and input[start_augend].isdigit():
+        start_augend -= 1
+
+    # Calculate the start_augend
+    augend = int(input[start_augend:index_of_add])
+
+    # Find the end of the addend
+    end_addend = index_of_add + 1
+    while end_addend < len(input) and input[end_addend].isdigit():
+        end_addend += 1
+
+    # Calculate the end of the end_addend
+    addend = int(input[index_of_add+1:end_addend])
+
+    # Recursively call the method, replacing the calculated part with the answer
+    return multiply_string(input[:start_augend] + str(augend + addend)
+        + input[end_addend:])
