f229a4996ee172a5749a48ada2b6b661126202f9
Joe <handjo@tcd.ie>
2022-10-06 10:49:54 +0100

    Subtraction-Implementation
    Non-finalised implementation of subtraction, along with handling of negative values

--- calculator.py
+++ calculator.py
@@ -1,10 +1,17 @@
 def calculate(input):
     answer = multiply_string(input)
     answer = add_string(answer)
+    answer = subtract_string(answer)
+    # replace special string "neg" with '-' once all operation have been calculated
+    print(answer)
+    answer = answer.replace("neg", "-")
+    print(answer)
+    answer = answer.replace('-','', answer.count('-')-1)
     return int(answer)
 
 
 def multiply_string(input):
+    print(input)
     index_of_mul = input.find("*")
 
     # If there is no * in the equation
@@ -13,13 +20,23 @@ def multiply_string(input):
 
     # Find the start of the multiplicand
     start_multiplicand = index_of_mul - 1
-    while start_multiplicand > 0 and input[start_multiplicand].isdigit():
+    while start_multiplicand > 0 and input[start_multiplicand-1].isdigit():
         start_multiplicand -= 1
     # Calculate the multiplicand
     multiplicand = int(input[start_multiplicand:index_of_mul])
 
+    if((found_negative(input[:index_of_mul], multiplicand))==1 and input[0]=='-'):
+        multiplicand = -multiplicand
+        #input = input.replace(str(input[start_multiplicand-1]),'')
+        #index_of_mul -= 1
+
     # Find the end of the multiplier
     end_multiplier = index_of_mul + 1
+
+    if(input[end_multiplier]=='-'):
+        end_multiplier += 1
+
+
     while end_multiplier < len(input) and input[end_multiplier].isdigit():
         end_multiplier += 1
     # Calculate the multiplier
@@ -31,6 +48,7 @@ def multiply_string(input):
 
 
 def add_string(input):
+    print(input)
     # using similar naming convention as the multiplication function
     index_of_add = input.find("+")
 
@@ -40,20 +58,89 @@ def add_string(input):
 
     # Find the augend (number to which another is added)
     start_augend = index_of_add - 1
-    while start_augend > 0 and input[start_augend].isdigit():
+    while start_augend > 0 and input[start_augend-1].isdigit():
         start_augend -= 1
 
     # Calculate the start_augend
     augend = int(input[start_augend:index_of_add])
 
+    if((found_negative(input[:index_of_add], augend))==1 and input[0]=='-'):
+        augend = -augend
+        #input = input.replace(str(input[start_augend-1]),'')
+        #index_of_add -= 1
+
     # Find the end of the addend
     end_addend = index_of_add + 1
+    if (input[end_addend]=='-'):
+        end_addend += 1
+
     while end_addend < len(input) and input[end_addend].isdigit():
         end_addend += 1
 
     # Calculate the addend
     addend = int(input[index_of_add + 1:end_addend])
+    print(addend)
+
+    if(augend < 0 and augend + addend > 0):
+        print(input)
+        input = input.replace(str(input[start_augend-1]),'')
+        print(input)
+        start_augend -= 1
+
 
     # Recursively call the method, replacing the calculated part with the answer
     return add_string(input[:start_augend] + str(augend + addend)
                       + input[end_addend:])
+
+def subtract_string(input):
+    print(input)
+    index_of_sub = input.find("-")
+
+    # If no subtraction operations are found
+    if index_of_sub == -1:
+        return input
+
+    # Find the start of the minuend
+    start_minuend = index_of_sub - 1
+    while start_minuend > 0 and input[start_minuend-1].isdigit():
+        start_minuend -= 1
+
+    # Calculate the start_minuend
+    if(input[start_minuend:index_of_sub]=='' or input[start_minuend:index_of_sub]=='g'):
+        input = input.replace('-',"neg",1)
+        return subtract_string(input)
+    else:
+        minuend = int(input[start_minuend:index_of_sub])
+
+    negminuend = "neg" + str(minuend)
+    if (input.find(negminuend)!=-1):
+        print("foundneg")
+        minuend = -minuend
+
+    end_subtrahend = index_of_sub + 1
+
+    if(input[end_subtrahend]=='-'):
+        end_subtrahend += 1
+    while end_subtrahend < len(input) and input[end_subtrahend].isdigit():
+        end_subtrahend += 1
+
+    # Calculate the end_subtrahend
+    subtrahend = int(input[index_of_sub + 1:end_subtrahend])
+    print(subtrahend)
+
+    # Recursively call the method, replacing the calculaed part with the answer
+    if (minuend - subtrahend < 0):
+        return subtract_string(input[:start_minuend] + str("neg" + str(abs(minuend - subtrahend)))
+                            + input[end_subtrahend:])
+    else:
+        print("else")
+        if (input[start_minuend-1]=='g' and minuend - subtrahend > 0):
+            input = input.replace("neg",'')
+        return subtract_string(input[:start_minuend] + str(abs(minuend - subtrahend))
+                            + input[end_subtrahend:])
+
+def found_negative(input, number):
+    if(input.find('-'+str(number))!=-1):
+        return 1
+    else:
+        return -1
--- testing.py
+++ testing.py
@@ -11,3 +11,22 @@ def test_addition():
     assert calculate("43+377")==420
     assert calculate("0+0")==0
     assert calculate("1+2+3+4+5")==15
+
+def test_subtraction():
+    assert calculate("2-1")==1
+    assert calculate("1-2")==-1
+    assert calculate("1-2-3-4-5")==-13
+
+def test_multi_op():
+    assert calculate("1+2*3")==7
+    assert calculate("1+2-3")==0
+    assert calculate("3*4-1")==11
+    assert calculate("8+6-7*4+3-2*6")==-23
+
+def test_neg_values():
+    assert calculate("-1+2")==1
+    assert calculate("1+-2")==-1
+    assert calculate("-4*5")==-20
+    assert calculate("5*-4")==-20
+    assert calculate("1--2")==3
+    assert calculate("-1--2")==1
