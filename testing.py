b3b3f45146cad172a74b698a1cc4cc837e1e98a7
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2022-10-06 11:21:24 +0100

    Add more testing for multiplication to confirm it works correctly

--- testing.py
+++ testing.py
@@ -8,6 +8,7 @@ def test_multiply():
     assert calculate("-1*20")==-20
     assert calculate("-5*-5")==25
     assert calculate("7*-6")==-42
+    assert calculate("120*-82")==-9840
 
 def test_addition():
     assert calculate("1+2")==3
@@ -21,3 +22,4 @@ def test_multiply_string():
     assert multiply_string("1+2*-3")=="1+-6"
     assert multiply_string("-2*3")=="-6"
     assert multiply_string("1-2*-3")=="1--6"
+    assert multiply_string("136+120*-82")=="136+-9840"
